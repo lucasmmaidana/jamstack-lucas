bc0512d40932daefadd45d9c6b6ae66b47585027
lucasmmaidana <lucasmaidanasoy@gmail.com>
2020-09-23 11:49:22 -0300

    first commit

---
+++ src/components/route-base.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const RoutBase = () => <h1>All your base belong to us!</h1>;
+export default RoutBase;
---
+++ src/components/route-secret.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const RoutSecret = () => <h1>This is super secret stuff.</h1>;
+export default RoutSecret;
